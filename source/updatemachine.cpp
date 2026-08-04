5c3d7bcb37838dfe61d07c3c00658d1a5c26fa20
oommk233 <lastsky_ok@163.com>
2026-08-04 09:39:10 +0800

    daily commit

--- source/updatemachine.cpp
+++ source/updatemachine.cpp
@@ -1,6 +1,23 @@
 #include "updatemachine.h"
 
-UpdateMachine::UpdateMachine()
+UpdateMachine::UpdateMachine():
+    objectsManager_(nullptr)
+{
+    initialization();
+}
+
+void UpdateMachine::updateWholeWorld()
 {
 
+}
+
+
+void UpdateMachine::initialization()
+{
+    ObjectsManager * obj = InstanceManager::getInstanceManager()->getObjectsManager();
+
+    if (obj != nullptr)
+    {
+        objectsManager_ = obj;
+    }
 }
--- source/updatemachine.h
+++ source/updatemachine.h
@@ -1,15 +1,27 @@
 #ifndef UPDATEMACHINE_H
 #define UPDATEMACHINE_H
 
+#include "ObjectsManager/objectsmanager.h"
+#include "ObjectsManager/instancemanager.h"
 
 class UpdateMachine
 {
 public:
     UpdateMachine();
 
+    void updateWholeWorld();
+
 private:
 
-    void intialization();
+    void initialization();
+
+    // To have all Objects inside, or let's say those need to be upated.
+    // But, shall not differenient the objects need to be updated or not,
+    // otherwise,is not good for the codes extention.
+
+    // In trial version, just put the manager links here inside
+    // It can be improved later, but now make the trial version work is the highest priority
+    ObjectsManager * objectsManager_;
 
 };
 
